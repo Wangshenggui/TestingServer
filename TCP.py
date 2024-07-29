66dd8ef8ccdd9bad65cd677d37149378fa7939ff
公司电脑 <@qq.com>
2024-07-29 11:33:03 +0800

    test

--- TCP.py
+++ TCP.py
@@ -12,9 +12,6 @@ async def handle_client(reader, writer):
 
             message = data.decode()  # 解码接收到的数据
             print(f"收到来自 {addr} 的消息: {message}")
-            
-            # 发送消息到 WebSocket 服务器
-            await send_message_to_websocket(message)
 
             # 可选：你可以在这里处理收到的数据或向客户端发送响应
             response = f"消息 '{message}' 已收到"
@@ -27,21 +24,9 @@ async def handle_client(reader, writer):
         #writer.close()
         await writer.wait_closed()
         
-# 发送消息到 WebSocket 服务器
-async def send_message_to_websocket(message):
-    if websocket_connection:
-        try:
-            print(f"发送消息到 WebSocket 服务器: {message}")
-            await websocket_connection.send(message)
-        except Exception as e:
-            print(f"发送消息到 WebSocket 服务器时发生错误: {e}")
 
 async def main():
 
-    addr = server.sockets[0].getsockname()
-    print(f"服务器已启动，监听在 {addr}")
-    
-    
     # 设置服务器监听所有接口的端口8000
     server = await asyncio.start_server(handle_client, '0.0.0.0',8000)
 
