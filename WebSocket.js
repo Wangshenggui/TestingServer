ae2d94ccd0f5dc7a4f56902cabd7271a88950d99
公司电脑 <@qq.com>
2024-07-29 10:57:51 +0800

    修改WS监听端口号

--- WebSocket.js
+++ WebSocket.js
@@ -42,8 +42,8 @@ const broadcastMessage = (message) => {
     }
 };
 
-const wss = new WebSocket.Server({ port: 8880 });
+const wss = new WebSocket.Server({ port: 8000 });
 
 wss.on('connection', handleClient);
 
-console.log("WebSocket 服务器已启动，监听在 172.29.103.118:8880");
+console.log("WebSocket 服务器已启动，监听在 172.29.103.118:8000");
